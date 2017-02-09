321f4e86323e0533b6fc0fcb81145b22f7856890
Unknown <admin@lucidlynx.net>
2017-02-09 17:02:52 -0500

    Add VS C++ Project Flies

---
+++ OS-Project.cpp
@@ -0,0 +1,14 @@
+// OS-Project.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	int test;
+	std::cout << "Hello world!" << std::endl;
+	std::cin >> test;
+    return 0;
+}
+
