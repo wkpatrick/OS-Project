f5fc363fb3a50731c316a3614e3f7863b477826e
Unknown <admin@lucidlynx.net>
2017-02-15 14:43:34 -0500

    Moving to desktop

--- Src/CPU.cpp
+++ Src/CPU.cpp
@@ -13,3 +13,40 @@ CPU::~CPU()
 {
 }
 
+void CPU::Execute(WORD opcode)
+{
+	WORD firstTwoBitsMask = 0b11000000000000000000000000000000;
+	WORD opcodeBitMask = 0b00111111000000000000000000000000;
+
+	WORD firstTwoBits = (opcode & firstTwoBits) >> 30;
+	WORD opcodeResult = (opcode & opcodeBitMask) >> 24;
+
+	switch (firstTwoBits)
+	{
+	case 0:
+		WORD firstSRegBitMask = 0b00000000111100000000000000000000;
+		WORD secondSRegBitMask = 0b00000000000011110000000000000000;
+		WORD dRegBitMask = 0b00000000000011110000000000000000;
+
+		WORD firstSReg = (opcode & firstSRegBitMask) >> 20;
+		WORD secondSReg = (opcode & secondSRegBitMask) >> 16;
+		WORD destReg = (opcode & dRegBitMask) >> 12;
+
+		break;
+	case 1:
+		WORD baseRegBitMask = 0b00000000111100000000000000000000;
+		WORD destRegBitMask = 0b00000000000011110000000000000000;
+		WORD addressBitMask = 0b00000000000000001111111111111111;
+
+		WORD baseReg = (opcode & baseRegBitMask) >> 20;
+		WORD destReg = (opcode & destRegBitMask) >> 16;
+		WORD address = (opcode & addressBitMask);
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	}
+
+}
+
--- Src/CPU.h
+++ Src/CPU.h
@@ -1,14 +1,25 @@
 #pragma once
+#include <vector>
 typedef unsigned long int WORD;
 typedef unsigned char BYTE;
+
+using namespace std;
 class CPU
 {
 	WORD Registers[16]; //16 32 bit registers, Reg[0] is the accumulator, Reg[1] is zero, and the rest are general purpose.
+	WORD ProgramCounter;
+	vector<WORD> InputBuffer;
+	vector<WORD> OutputBuffer;
+	vector<WORD> TempBuffer;
+
 public:
 	CPU();
 	~CPU();
 
 	int busy; // 0 is idle, 1 is busy
 	void BeginJob();
+	WORD GetNextWord();
+	void Execute(WORD word);
+
 };
 
