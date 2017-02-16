2e1a04702af8a5d039b4fd4281e040a4fc984d4d
Unknown <admin@lucidlynx.net>
2017-02-15 22:13:28 -0500

    Partial Implmentation of CPU opcodes
    Waiting for memory access to get finalized before I can do the rest.

--- Src/CPU.cpp
+++ Src/CPU.cpp
@@ -24,6 +24,7 @@ void CPU::Execute(WORD opcode)
 	switch (firstTwoBits)
 	{
 	case 0:
+	{
 		WORD firstSRegBitMask = 0b00000000111100000000000000000000;
 		WORD secondSRegBitMask = 0b00000000000011110000000000000000;
 		WORD dRegBitMask = 0b00000000000011110000000000000000;
@@ -31,9 +32,10 @@ void CPU::Execute(WORD opcode)
 		WORD firstSReg = (opcode & firstSRegBitMask) >> 20;
 		WORD secondSReg = (opcode & secondSRegBitMask) >> 16;
 		WORD destReg = (opcode & dRegBitMask) >> 12;
-
+	}
 		break;
 	case 1:
+	{
 		WORD baseRegBitMask = 0b00000000111100000000000000000000;
 		WORD destRegBitMask = 0b00000000000011110000000000000000;
 		WORD addressBitMask = 0b00000000000000001111111111111111;
@@ -41,12 +43,210 @@ void CPU::Execute(WORD opcode)
 		WORD baseReg = (opcode & baseRegBitMask) >> 20;
 		WORD destReg = (opcode & destRegBitMask) >> 16;
 		WORD address = (opcode & addressBitMask);
+	}
 		break;
 	case 2:
+	{
+		WORD addressBitMask = 0b00000000111111111111111111111111;
+		WORD adress = (opcode & addressBitMask);
+	}
 		break;
 	case 3:
+	{
+		WORD regOneBitMask = 0b00000000111100000000000000000000;
+		WORD regTwoBitMask = 0b00000000000011110000000000000000;
+		WORD addressBitMask = 0b00000000000000001111111111111111;
+
+		WORD regOne = (opcode & regOneBitMask) >> 20;
+		WORD regTwo = (opcode & regTwoBitMask) >> 16;
+		WORD address = (opcode & addressBitMask);
+	}
 		break;
 	}
 
+}
+
+void CPU::OPCode00(WORD opcode)
+{
+	WORD regOneBitMask = 0b00000000111100000000000000000000;
+	WORD regTwoBitMask = 0b00000000000011110000000000000000;
+	WORD addressBitMask = 0b00000000000000001111111111111111;
+
+	WORD regOne = (opcode & regOneBitMask) >> 20;
+	WORD regTwo = (opcode & regTwoBitMask) >> 16;
+	WORD address = (opcode & addressBitMask);
+
+	Registers[regOne] = InputBuffer.back();
+	InputBuffer.pop_back();
+}
+
+void CPU::OPCode01(WORD opcode)
+{
+	WORD regOneBitMask = 0b00000000111100000000000000000000;
+	WORD regTwoBitMask = 0b00000000000011110000000000000000;
+	WORD addressBitMask = 0b00000000000000001111111111111111;
+
+	WORD regOne = (opcode & regOneBitMask) >> 20;
+	WORD regTwo = (opcode & regTwoBitMask) >> 16;
+	WORD address = (opcode & addressBitMask);
+
+	InputBuffer.push_back(Registers[regOne]);
+}
+
+void CPU::OPCode02(WORD opcode)
+{
+	//TODO   Need The GateKeeper Implemented
+}
+
+void CPU::OPCode03(WORD opcode)
+{
+	//TODO Need The GateKeeper Implmented
+}
+
+void CPU::OPCode04(WORD opcode)
+{
+
+}
+
+void CPU::OPCode05(WORD opcode)
+{
+	WORD firstSRegBitMask = 0b00000000111100000000000000000000;
+	WORD secondSRegBitMask = 0b00000000000011110000000000000000;
+	WORD dRegBitMask = 0b00000000000011110000000000000000;
+
+	WORD firstSReg = (opcode & firstSRegBitMask) >> 20;
+	WORD secondSReg = (opcode & secondSRegBitMask) >> 16;
+	WORD destReg = (opcode & dRegBitMask) >> 12;
+
+	Registers[destReg] = Registers[firstSReg] + Registers[secondSReg];
+}
+
+void CPU::OPCode06(WORD opcode)
+{
+	WORD firstSRegBitMask = 0b00000000111100000000000000000000;
+	WORD secondSRegBitMask = 0b00000000000011110000000000000000;
+	WORD dRegBitMask = 0b00000000000011110000000000000000;
+
+	WORD firstSReg = (opcode & firstSRegBitMask) >> 20;
+	WORD secondSReg = (opcode & secondSRegBitMask) >> 16;
+	WORD destReg = (opcode & dRegBitMask) >> 12;
+
+	Registers[destReg] = Registers[firstSReg] - Registers[secondSReg];
+}
+
+void CPU::OPCode07(WORD opcode)
+{
+	WORD firstSRegBitMask = 0b00000000111100000000000000000000;
+	WORD secondSRegBitMask = 0b00000000000011110000000000000000;
+	WORD dRegBitMask = 0b00000000000011110000000000000000;
+
+	WORD firstSReg = (opcode & firstSRegBitMask) >> 20;
+	WORD secondSReg = (opcode & secondSRegBitMask) >> 16;
+	WORD destReg = (opcode & dRegBitMask) >> 12;
+
+	Registers[destReg] = Registers[firstSReg] * Registers[secondSReg];
+}
+
+void CPU::OPCode08(WORD opcode)
+{
+	WORD firstSRegBitMask = 0b00000000111100000000000000000000;
+	WORD secondSRegBitMask = 0b00000000000011110000000000000000;
+	WORD dRegBitMask = 0b00000000000011110000000000000000;
+
+	WORD firstSReg = (opcode & firstSRegBitMask) >> 20;
+	WORD secondSReg = (opcode & secondSRegBitMask) >> 16;
+	WORD destReg = (opcode & dRegBitMask) >> 12;
+
+	Registers[destReg] = Registers[firstSReg] / Registers[secondSReg];
+}
+
+void CPU::OPCode09(WORD opcode)
+{
+	WORD firstSRegBitMask = 0b00000000111100000000000000000000;
+	WORD secondSRegBitMask = 0b00000000000011110000000000000000;
+	WORD dRegBitMask = 0b00000000000011110000000000000000;
+
+	WORD firstSReg = (opcode & firstSRegBitMask) >> 20;
+	WORD secondSReg = (opcode & secondSRegBitMask) >> 16;
+	WORD destReg = (opcode & dRegBitMask) >> 12;
+
+	Registers[destReg] = Registers[firstSReg] & Registers[secondSReg];
+}
+
+void CPU::OPCode0A(WORD opcode)
+{
+	WORD firstSRegBitMask = 0b00000000111100000000000000000000;
+	WORD secondSRegBitMask = 0b00000000000011110000000000000000;
+	WORD dRegBitMask = 0b00000000000011110000000000000000;
+
+	WORD firstSReg = (opcode & firstSRegBitMask) >> 20;
+	WORD secondSReg = (opcode & secondSRegBitMask) >> 16;
+	WORD destReg = (opcode & dRegBitMask) >> 12;
+
+	Registers[destReg] = Registers[firstSReg] | Registers[secondSReg];
+}
+
+void CPU::OPCode0B(WORD opcode)
+{
+}
+
+void CPU::OPCode0C(WORD opcode)
+{
+}
+
+void CPU::OPCode0D(WORD opcode)
+{
+}
+
+void CPU::OPCode0E(WORD opcode)
+{
+}
+
+void CPU::OPCode0F(WORD opcode)
+{
+}
+
+void CPU::OPCode10(WORD opcode)
+{
+}
+
+void CPU::OPCode11(WORD opcode)
+{
+}
+
+void CPU::OPCode12(WORD opcode)
+{
+}
+
+void CPU::OPCode13(WORD opcode)
+{
+}
+
+void CPU::OPCode14(WORD opcode)
+{
+}
+
+void CPU::OPCode15(WORD opcode)
+{
+}
+
+void CPU::OPCode16(WORD opcode)
+{
+}
+
+void CPU::OPCode17(WORD opcode)
+{
+}
+
+void CPU::OPCode18(WORD opcode)
+{
+}
+
+void CPU::OPCode19(WORD opcode)
+{
+}
+
+void CPU::OPCode1A(WORD opcode)
+{
 }
 
--- Src/CPU.h
+++ Src/CPU.h
@@ -21,5 +21,33 @@ public:
 	WORD GetNextWord();
 	void Execute(WORD word);
 
+private:
+	void OPCode00(WORD opcode);
+	void OPCode01(WORD opcode);
+	void OPCode02(WORD opcode);
+	void OPCode03(WORD opcode);
+	void OPCode04(WORD opcode);
+	void OPCode05(WORD opcode);
+	void OPCode06(WORD opcode);
+	void OPCode07(WORD opcode);
+	void OPCode08(WORD opcode);
+	void OPCode09(WORD opcode);
+	void OPCode0A(WORD opcode);
+	void OPCode0B(WORD opcode);
+	void OPCode0C(WORD opcode);
+	void OPCode0D(WORD opcode);
+	void OPCode0E(WORD opcode);
+	void OPCode0F(WORD opcode);
+	void OPCode10(WORD opcode);
+	void OPCode11(WORD opcode);
+	void OPCode12(WORD opcode);
+	void OPCode13(WORD opcode);
+	void OPCode14(WORD opcode);
+	void OPCode15(WORD opcode);
+	void OPCode16(WORD opcode);
+	void OPCode17(WORD opcode);
+	void OPCode18(WORD opcode);
+	void OPCode19(WORD opcode);
+	void OPCode1A(WORD opcode);
 };
 
